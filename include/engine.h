d9df4edbb1deec4d5f6c1af833f2cb58d78ce063
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-08-16 22:13:49 +0700

    feat: process_request after accepting client, and get_endpoint first line

--- include/engine.h
+++ include/engine.h
@@ -2,13 +2,17 @@
 #define ENGINE
 
 #include <stdint.h>
+#include <stddef.h>
 
 struct prescal_engine{
     char *host;
     uint16_t port;
 };
 
+/* Prescal Engine function Definition */
 struct prescal_engine *engine_init(char *host, uint16_t port);
 void start(struct prescal_engine *engine);
+void process_request(int fd);
+void get_endpoint(const char *src, char *out, size_t size);
 
 #endif
