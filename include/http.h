9ce1d9b7a59e7ca69f9bae705749a2b5a07781dc
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-08-20 22:12:14 +0700

    feat: adding function to clear entire memory of http_req

--- include/http.h
+++ include/http.h
@@ -7,6 +7,7 @@ struct http_req {
     char *method;
     char *path;
     char *http_v;
+    char *raw;
 };
 
 /* http req function definitions */
@@ -15,5 +16,6 @@ void extract_req(struct http_req *hreq, char *endpoint_info);
 void req_to_string(struct http_req *req);
 void convert_request(struct http_req *req, const char *request, size_t size);
 void get_endpoint_info(const char *src, char *out, size_t size);
+void http_req_clean(struct http_req *hreq);
 
 #endif
