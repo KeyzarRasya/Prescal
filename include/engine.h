7bbe5cc5822abc90e9e1a83d03665de4606ee5ee
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-08-20 22:11:15 +0700

    refactor: changing forwards return

--- include/engine.h
+++ include/engine.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "configuration.h"
+
+#define MAX_BUFF_SIZE   1024
+#define BACKEND_PORT    3000
 
 #define ON_SOCK_ERR \
     "HTTP/1.1 502 Bad Gateway\r\n" \
@@ -15,12 +19,14 @@
 struct prescal_engine{
     char *host;
     uint16_t port;
+    struct prescal_config *config;
 };
 
 /* Prescal Engine function Definition */
 struct prescal_engine *engine_init(char *host, uint16_t port);
 void start(struct prescal_engine *engine);
 void process_request(int fd);
-void forwards(const char *request, char *http_response, size_t size);
+int forwards(const char *request, char *http_response, size_t size);
+void destroy_engine(struct prescal_engine *engine);
 
 #endif
