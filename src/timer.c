8068e976c90b3ccab2a0f64dde9db3cdcc8b6512
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-08-20 22:51:37 +0700

    feat: sleep_ms function that wait until parameterize ms

---
+++ src/timer.c
@@ -0,0 +1,10 @@
+#include "../include/timer.h"
+#include <unistd.h>
+#include <time.h>
+
+void sleep_ms(long ms) {
+    struct timespec ts;
+    ts.tv_sec = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000L;
+    nanosleep(&ts, NULL);
+}
