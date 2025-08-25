0e3e4adc336e193b5e81e4c7b8486933e77c3ddc
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-08-25 13:04:37 +0700

    feat: adding threads for calculate and measure metrics

--- src/engine.c
+++ src/engine.c
@@ -13,17 +13,47 @@
 #include <pthread.h>
 #include <stdatomic.h>
 #include "timer.h"
+#include "metrics.h"
 
-#define DEBUG 0
-
-atomic_int rps = ATOMIC_VAR_INIT(0);
+#define DEBUG 0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+atomic_int rps_a = ATOMIC_VAR_INIT(0);
+double cpu_t0;
 
 void *request_per_second() {
+    FILE *fptr = fopen("/tmp/data.txt", "a");
+    if (!fptr) {
+        perror("file pointer");
+        return NULL;
+    }
+
+    char metrics_res[MAX_METRICS_RESPONSE];
+    if (request_metrics(3000, metrics_res, sizeof(metrics_res)) != 0) {
+        perror("CPU t0 initial");
+        return NULL;
+    }
+    cpu_t0 = get_value(metrics_res, CPU_SECOND);
+
     while(1) {
         sleep_ms(1000);
-        int current_rps = atomic_exchange(&rps, 0);
-        printf("RPS : %d\n", current_rps);
+        struct metrics *metrics = init_metrics();
+        if (request_metrics(3000, metrics_res, sizeof(metrics_res)) != 0) {
+            perror("metrics request");
+            break;
+        }
+        store_metrics(metrics, metrics_res);
+
+        double current_cpu = metrics->cpu_usage;
+        calculate_metrics(metrics, cpu_t0);
+
+        int rps = atomic_exchange(&rps_a, 0);
+        write_metrics(fptr, metrics, rps);
+        cpu_t0 = current_cpu;
+        rps = 0;
+        free(metrics);
+        memset(metrics_res, 0, sizeof(metrics_res));
     }
+    fclose(fptr);
+    return NULL;
 }
 
 int handle_request(int fd, struct http_req *hreq) {
@@ -49,7 +79,6 @@ void handle_response(int fd, struct http_req *hreq) {
 void log_elapsed_time(struct timespec start, struct timespec end) {
     double elapsed = (end.tv_sec - start.tv_sec) +
                      (end.tv_nsec - start.tv_nsec) / 1e9;
-    int request = atomic_load(&rps);
     printf("Elapsed time: %0.6f\n", elapsed);
 }
 
@@ -85,11 +114,11 @@ struct prescal_engine *engine_init(char *host, uint16_t port) {
 }
 
 void start(struct prescal_engine *engine) {
-    struct sockaddr_in client_addr;
     pthread_t rps_reset_thread;
-
     pthread_create(&rps_reset_thread, NULL, request_per_second, NULL);
+    
     int fd;
+    struct sockaddr_in client_addr;
 
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
@@ -138,12 +167,12 @@ void process_request(int fd) {
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     struct http_req *hreq = http_req_init();
-    atomic_fetch_add(&rps, 1);
 
     if (handle_request(fd, hreq) != 0) {
         free(hreq);
         return;
     }
+    atomic_fetch_add(&rps_a, 1);
 
     handle_response(fd, hreq);
     clock_gettime(CLOCK_MONOTONIC, &end);
